6118e18b03d0753c5a14937d02b1ddfab87d654b
programingbigger <shidiannaigong@gmail.com>
2026-03-16 23:54:06 +0900

    基本的なファイルをアップロード

---
+++ OmikujiApp/OmikujiApp/OmikujiAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  OmikujiAppApp.swift
+//  OmikujiApp
+//
+//  Created by 奈宮史典 on 2026/03/16.
+//
+
+import SwiftUI
+
+@main
+struct OmikujiAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
